8af53bd94da482ead50d5450e90a275d43898d8d
mohammad osama <okosama06@gmail.com>
2023-08-04 02:00:26 +0530

    Real Estate all page done

---
+++ src/routes/Home.jsx
@@ -0,0 +1,14 @@
+
+import Search from '../components/Search/Search'
+import HouseList from '../components/Houses/HouseList';
+
+const Home = () => {
+  return (
+    <>
+      <Search />
+      <HouseList />
+    </>
+  )
+}
+
+export default Home;
